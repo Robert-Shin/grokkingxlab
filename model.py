a33b39ce8c8ba566f1aea0cac7e46eb6d1d44d80
Robert-Shin <35202617+Robert-Shin@users.noreply.github.com>
2024-08-05 15:13:23 -0500

    Update model.py
    Changed sampling to argmax

--- model.py
+++ model.py
@@ -333,7 +333,8 @@ class GPT(nn.Module):
             # apply softmax to convert logits to (normalized) probabilities
             probs = F.softmax(logits, dim=-1)
             # sample from the distribution
-            idx_next = torch.multinomial(probs, num_samples=1)
+            #idx_next = torch.multinomial(probs, num_samples=1)
+            idx_next = torch.argmax(probs, dim=-1, keepdim=True)
             # append sampled index to the running sequence and continue
             idx = torch.cat((idx, idx_next), dim=1)
 
