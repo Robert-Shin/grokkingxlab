eabfa49b2a25165c8f1b4d49047ae8ae0b13fda7
Robert-Shin <35202617+Robert-Shin@users.noreply.github.com>
2024-08-06 16:26:35 -0500

    Update my_utils.py

--- my_utils.py
+++ my_utils.py
@@ -1,5 +1,6 @@
 
 import torch as t
+from torch import Tensor
 import einops
 import torch.nn.functional as F
 import plotly.express as px
@@ -9,6 +10,7 @@ import numpy as np
 import pandas as pd
 from IPython.display import display
 from typing import Tuple, List, Callable
+from jaxtyping import Float, Int
 from transformer_lens import HookedTransformer, utils
 from transformer_lens.hook_points import HookPoint
 import random
@@ -397,6 +399,132 @@ def get_component_sin_xpy(tensor, freq, collapse_dim=False):
         return sin_xpy_direction[:, None] @ (sin_xpy_direction[None, :] @ tensor)
 
 
+def arrange_by_2d_freqs(tensor):
+    '''
+    Takes a tensor of shape (p, p, ...) and returns a tensor of shape
+    (p//2 - 1, 3, 3, ...) representing the Fourier coefficients sorted by
+    frequency (each slice contains const, linear and quadratic terms).
+
+    In other words, if the first two dimensions of the original tensor
+    correspond to indexing by 2D Fourier frequencies as follows:
+
+        1           cos(w_1*x)            sin(w_1*x)           ...
+        cos(w_1*y)  cos(w_1*x)cos(w_1*y)  sin(w_1*x)cos(w_1*y) ...
+        sin(w_1*y)  cos(w_1*x)sin(w_1*y)  sin(w_1*x)sin(w_1*y) ...
+        cos(w_2*y)  cos(w_1*x)cos(w_2*y)  sin(w_1*x)cos(w_2*y) ...
+        ...
+
+    Then the (k-1)-th slice of the new tensor are the terms corresponding to
+    the following 2D Fourier frequencies:
+
+        1           cos(w_k*x)            sin(w_k*x)           ...
+        cos(w_k*y)  cos(w_k*x)cos(w_k*y)  sin(w_k*x)cos(w_k*y) ...
+        sin(w_k*y)  cos(w_k*x)sin(w_k*y)  sin(w_k*x)sin(w_k*y) ...
+
+    for k = 1, 2, ..., p//2.
+
+    Note we omit the constant term, i.e. the 0th slice has frequency k=1.
+    '''
+    idx_2d_y_all = []
+    idx_2d_x_all = []
+    for freq in range(1, p//2):
+        idx_1d = [0, 2*freq-1, 2*freq]
+        idx_2d_x_all.append([idx_1d for _ in range(3)])
+        idx_2d_y_all.append([[i]*3 for i in idx_1d])
+    return tensor[idx_2d_y_all, idx_2d_x_all]
+
+
+# Note the use of `einops.reduce` in the solution, rather than just using e.g.
+# `fourier_neuron_acts.pow(2).sum((0, 1)). Like most of the situations where
+# `einops` is helpful, this has the advantage of making your code more explicit,
+# readable, and reduces the chance of mistakes.
+
+
+def find_neuron_freqs(
+    fourier_neuron_acts):
+    '''
+    Returns the tensors `neuron_freqs` and `neuron_frac_explained`,
+    containing the frequencies that explain the most variance of each
+    neuron and the fraction of variance explained, respectively.
+    '''
+    fourier_neuron_acts_by_freq = arrange_by_2d_freqs(fourier_neuron_acts)
+    assert fourier_neuron_acts_by_freq.shape == (p//2-1, 3, 3, d_mlp)
+
+    # SOLUTION
+    # Sum squares of all frequency coeffs, for each neuron
+    square_of_all_terms = einops.reduce(
+        fourier_neuron_acts.pow(2),
+        "x_coeff y_coeff neuron -> neuron",
+        "sum"
+    )
+
+    # Sum squares just corresponding to const+linear+quadratic terms,
+    # for each frequency, for each neuron
+    square_of_each_freq = einops.reduce(
+        fourier_neuron_acts_by_freq.pow(2),
+        "freq x_coeff y_coeff neuron -> freq neuron",
+        "sum"
+    )
+
+    # Find the freq explaining most variance for each neuron
+    # (and the fraction of variance explained)
+    neuron_variance_explained, neuron_freqs = square_of_each_freq.max(0)
+    neuron_frac_explained = neuron_variance_explained / square_of_all_terms
+
+    # The actual frequencies count up from k=1, not 0!
+    neuron_freqs += 1
+
+    return neuron_freqs, neuron_frac_explained
+
+
+def project_onto_direction(batch_vecs: t.Tensor, v: t.Tensor) -> t.Tensor:
+    '''
+    Returns the component of each vector in `batch_vecs` in the direction of `v`.
+
+    batch_vecs.shape = (n, ...)
+    v.shape = (n,)
+    '''
+    return v[:, None] @ (v @ batch_vecs)[None, ...]
+
+def project_onto_frequency(batch_vecs: t.Tensor, freq: int) -> t.Tensor:
+    '''
+    Returns the projection of each vector in `batch_vecs` onto the
+    2D Fourier basis directions corresponding to frequency `freq`.
+
+    batch_vecs.shape = (p**2, ...)
+    '''
+    assert batch_vecs.shape[0] == p**2
+    # SOLUTION
+
+    return sum([
+        project_onto_direction(
+            batch_vecs,
+            fourier_2d_basis_term(i, j).flatten(),
+        )
+        for i in [0, 2*freq-1, 2*freq] for j in [0, 2*freq-1, 2*freq]
+    ])
+
+def get_trig_sum_directions(k: int) -> Tuple[Float[Tensor, "p p"], Float[Tensor, "p p"]]:
+    '''
+    Given frequency k, returns the normalized vectors in the 2D Fourier basis
+    representing the directions:
+
+        cos(ω_k * (x + y))
+        sin(ω_k * (x + y))
+
+    respectively.
+    '''
+    # SOLUTION
+    cosx_cosy_direction = fourier_2d_basis_term(2*k-1, 2*k-1)
+    sinx_siny_direction = fourier_2d_basis_term(2*k, 2*k)
+    sinx_cosy_direction = fourier_2d_basis_term(2*k, 2*k-1)
+    cosx_siny_direction = fourier_2d_basis_term(2*k-1, 2*k)
+
+    cos_xplusy_direction = (cosx_cosy_direction - sinx_siny_direction) / np.sqrt(2)
+    sin_xplusy_direction = (sinx_cosy_direction + cosx_siny_direction) / np.sqrt(2)
+
+    return cos_xplusy_direction, sin_xplusy_direction
+
 
 
 def load_in_state_dict(model, state_dict):
@@ -426,7 +554,42 @@ def load_in_state_dict(model, state_dict):
     model.load_state_dict(state_dict_new)
     return model
 
-
+def modified_load_in_state_dict(model, state_dict):
+    """
+    Loads in state dict, modified for nanogpt
+    """
+    state_dict_new = model.state_dict()
+    for k, v in state_dict_new.items():
+        print(k, v.shape)
+    for k, v in state_dict.items():
+        if 'wte' in k:
+            state_dict_new["embed.W_E"] = v
+        elif 'wpe' in k:
+            state_dict_new["pos_embed.W_pos"] = v
+        elif "lm_head" in k:
+            state_dict_new["unembed.W_U"] = v.transpose(-1, -2)
+        elif "mlp.c_proj" in k:
+            state_dict_new["blocks.0.mlp.W_out"] = v.transpose(-1, -2)
+        elif "mlp.c_fc" in k:
+            state_dict_new["blocks.0.mlp.W_in"] = v.transpose(-1, -2)
+        elif "attn.c_proj" in k:
+            state_dict_new["blocks.0.attn.W_O"] = einops.rearrange(v, "d_model (n_heads d_head) -> n_heads d_head d_model", d_head=model.cfg.d_head)
+        elif "attn.c_attn" in k:
+            Q, K, V  = v.chunk(3, dim=0)
+            Q = Q.view(4, 32, 128).transpose(-1, -2)
+            K = K.view(4, 32, 128).transpose(-1, -2)
+            V = V.view(4, 32, 128).transpose(-1, -2)
+            state_dict_new["blocks.0.attn.W_Q"] = Q
+            state_dict_new["blocks.0.attn.W_K"] = K
+            state_dict_new["blocks.0.attn.W_V"] = V
+    # Make sure biases are zero
+    for k, v in state_dict_new.items():
+        if "b_" in k and "mlp" not in k:
+            state_dict_new[k] = t.zeros_like(v)
+    state_dict_new["blocks.0.attn.IGNORE"] = t.full_like(state_dict_new["blocks.0.attn.IGNORE"], -1e10)
+    
+    model.load_state_dict(state_dict_new)
+    return model
 
 
 # # Hate that I needed to do this lol
